f9688202c5bad1b0354211c1aa2eed8e46044378
Jean Fraga <jeandevjs@gmail.com>
2022-07-26 16:38:08 -0300

    add text fixture data | dispositivos de testes

---
+++ combination-logic-2/test-fixture-data.js
@@ -0,0 +1,66 @@
+export const produto = 'camisa'
+export const pedidos = [
+    { 
+        items: [ 
+            'calça', 
+            'camisa',
+            'bone',
+            "regata"
+        ] 
+    },
+    { 
+      items: [ 
+          'calça', 
+          'bone'
+      ] 
+  },
+    { 
+        items: [ 
+          'camisa',
+          'bone',
+          "chinelo",
+          'calça', 
+        ] 
+    },
+    { 
+        items: [ 
+            'calça', 
+            'camisa',  
+        ] 
+    },
+    { 
+        items: [  
+            'camisa',
+            'calça',
+            'sandália'
+        ] 
+    },
+    { 
+      items: [  
+          'camisa',
+          'calça',
+          'cordão'
+      ] 
+  },
+  { 
+    items: [  
+        'camisa',
+        'calça',
+        'cordão'
+    ] 
+},
+{ 
+  items: [  
+      'camisa',
+      'calça',
+      'cordão'
+  ] 
+},
+{ 
+  items: [  
+      'calça',
+      'cordão'
+  ] 
+},
+
+]
