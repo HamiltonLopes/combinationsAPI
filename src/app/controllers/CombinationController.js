f3a2013c614c7317009458204dde5071a246e28e
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-07-28 14:02:06 -0300

    adding topCombination's Logic

--- src/app/controllers/CombinationController.js
+++ src/app/controllers/CombinationController.js
@@ -14,6 +14,27 @@ export default new class RewardsController {
         const NUMBER_OF_TOP_COMBINATIONS = 3; //pegar do masterdata também?
         let { combinations, topCombinations } = await; //MasterDataget
 
+        const addTopCombination = (obj) =>{ //método para atualizar o vetor de melhores combinacoes
+            let addIndex = -1;
+            for(let j= 0; j < topCombinations.length; j++){
+                if(((+Object.values(Object.values(topCombinations[j])[0])[0] < +Object.keys(Object.values(obj)[0])[0]) && addIndex === -1)
+                    || ( (j === (topCombinations.length - 1)) && (+Object.values(Object.values(topCombinations[j])[0])[0] === +Object.keys(Object.values(obj)[0])[0]) && addIndex === -1)){
+                    addIndex = j;
+                }
+                if((Object.keys(obj)[0] === Object.keys(topCombinations[j])[0]) && (Object.values(Object.values(obj)[0])[0] === Object.keys(Object.values(topCombinations[j])[0])[0])){
+                    const newAparritionsValue = +Object.keys(Object.values(obj)[0])[0] + +Object.values(Object.values(topCombinations[j])[0])[0];
+                    topCombinations.splice(j,1);
+                    return topCombinations.splice(j, 0, {[Object.keys(obj)[0]]:{[Object.values(Object.values(obj)[0])[0]] : newAparritionsValue}});
+                }
+            }
+            if(addIndex !== -1){ 
+                if(topCombinations.length === NUMBER_OF_COMBINATIONS) topCombinations.splice((NUMBER_OF_COMBINATIONS-1),1);
+                return topCombinations.splice(addIndex, 0, {[Object.keys(obj)[0]]:{[Object.values(Object.values(obj)[0])[0]] : +Object.keys(Object.values(obj)[0])[0]}});
+            }else if(topCombinations.length < NUMBER_OF_COMBINATIONS){
+                return topCombinations.push({[Object.keys(obj)[0]]:{[Object.values(Object.values(obj)[0])[0]] : +Object.keys(Object.values(obj)[0])[0]}});
+            }
+        };
+
         if(items.length > 1){
             for(const item of items){ //percorre os items do pedido recebido (a partir daqui itemPrincipal)
                 if(!combinations[item]){ //verifica se já existe combinações com o itemPrincipal
@@ -29,20 +50,13 @@ export default new class RewardsController {
                         let isInserted = false; //variavel para saber se a combinacao existe e foi acrescentada uma aparição
                         for(let i = 0; i < combinations[item].combinations.length; i++){ //percorre a lista de combinações do itemPrincipal
                             if(Object.values(hashItem)[0] === subItem){ //verifica se existe a combinação do ItemPrincipal com o subItem
+                                const newApparitionsValue = +Object.keys(combinations[item].combinations[i])[0] + 1;
                                 combinations[item].combinations.push({  //se existir adiciona + 1 no número de aparições da combinação e insere no vetor de combinações
-                                    [+Object.keys(combinations[item].combinations[i])[0] + 1]: subItem
+                                    [newApparitionsValue]: subItem
                                 });
+                                addTopCombination({[item]: {[newApparitionsValue]: subItem}}); // adiciona a combinação nos tops, somando quando ela já existir ou verificando se a nova soma é superior
                                 combinations[item].combinations.splice(i, 1); //remove o número de combinações antigas(do itemPrincipal com o subItem) do vetor de combinações
                                 combinations[item].combinations = mergeSort(obj[item].combinations); //Ordena novamente as combinações para a primeira combinação sempre ser a com maior número de aparições
-
-                                /*
-                                for(const comb of topCombinations){
-                                    if()
-                                }
-    
-                                    INSERIR LÓGICA TOP COMBINAÇÕES
-                                
-                                */
                                 isInserted = true; //informa que foi acrescentado uma aparição 
                                 break;
                             }
@@ -51,6 +65,7 @@ export default new class RewardsController {
                             combinations[item].combinations.push({  //se a combinacao não existir, insere ela no fim da lista com valor de uma aparição
                                 ["1"]: subItem
                             });
+                            addTopCombination({[item]: {["1"]: subItem}}); //tenta adicionar no array de melhoes combinacoes
                         }
                     }
                 }
--- testes/teste2.js
+++ testes/teste2.js
@@ -11,9 +11,10 @@ var obj = {
     }
 }
 var subItem = "colarAzul";
+const NUMBER_OF_COMBINATIONS = 3;
 
 var item = 'camisaBrancaId';
-let topCombinations = [ {"camisa":{"calca":2}}, {"camisa":{"shorts":1}},{"camisa":{"bone":1}}];
+let topCombinations = [ ];
 
 const addTopCombination = (obj) =>{
     let addIndex = -1;
@@ -23,20 +24,27 @@ const addTopCombination = (obj) =>{
             addIndex = j;
         }
         if((Object.keys(obj)[0] === Object.keys(topCombinations[j])[0]) && (Object.values(Object.values(obj)[0])[0] === Object.keys(Object.values(topCombinations[j])[0])[0])){
+            const newAparritionsValue = +Object.keys(Object.values(obj)[0])[0] + +Object.values(Object.values(topCombinations[j])[0])[0];
             topCombinations.splice(j,1);
+            return topCombinations.splice(j, 0, {[Object.keys(obj)[0]]:{[Object.values(Object.values(obj)[0])[0]] : newAparritionsValue}});
         }
     }
-    console.log(addIndex)
-    if(addIndex !== -1) topCombinations.splice(addIndex, 0, {[Object.keys(obj)[0]]:{
-        [Object.values(Object.values(obj)[0])[0]] : +Object.keys(Object.values(obj)[0])[0]
-    }});
+    if(addIndex !== -1){ 
+        if(topCombinations.length === NUMBER_OF_COMBINATIONS) topCombinations.splice((NUMBER_OF_COMBINATIONS-1),1);
+        return topCombinations.splice(addIndex, 0, {[Object.keys(obj)[0]]:{[Object.values(Object.values(obj)[0])[0]] : +Object.keys(Object.values(obj)[0])[0]}});
+    }else if(topCombinations.length < NUMBER_OF_COMBINATIONS){
+        return topCombinations.push({[Object.keys(obj)[0]]:{[Object.values(Object.values(obj)[0])[0]] : +Object.keys(Object.values(obj)[0])[0]}});
+    }
 };
 
 let isInserted = false;
 for (let i = 0; i < obj[item].combinations.length; i++) {
     if (Object.values(obj[item].combinations[i])[0] === subItem) {
+        const newAparritionsValue = +Object.keys(obj[item].combinations[i])[0] + 1;
+        console.log({[item]: {[subItem]: newAparritionsValue}});
+        addTopCombination({[item]: {[subItem]: newAparritionsValue}});
         obj[item].combinations.push({
-            [+Object.keys(obj[item].combinations[i])[0] + 1]: subItem
+            [newAparritionsValue]: subItem
         });
         obj[item].combinations.splice(i, 1);
         obj[item].combinations = mergeSort(obj[item].combinations);
@@ -49,15 +57,18 @@ if(!isInserted){
     obj[item].combinations.push({
         [1]: subItem
     });
+    addTopCombination({[item]: {'1': [subItem]}});
 }
 
 obj[item].combinations = mergeSort(obj[item].combinations);
 
-console.log(obj[item].combinations);
+console.log(obj[item]);
 console.log(topCombinations);
-var obej = {"camisa":{"2":"bone"}};
-addTopCombination(obej);
-console.log(topCombinations);
+// var obej = {"camisa":{"1":"bone"}};
+// addTopCombination(obej);
+// addTopCombination(obej);
+// addTopCombination(obej);
+// console.log(topCombinations);
 
 
 
