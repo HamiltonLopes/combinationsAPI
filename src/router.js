5bb1fe3dc9dae54420388df7138a08f3a35a1ecf
Jean Fraga <jeandevjs@gmail.com>
2022-07-30 17:32:37 -0300

    feat: add ControllerCombinationsById

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import dotenv from 'dotenv';
 import CombinationController from './app/controllers/CombinationController.js'
+import { ControllerCombinationsById } from '../src/app/controllers/controller-combinations-by-id.js'
 dotenv.config();
 
 const Routes = new Router();
@@ -8,5 +9,6 @@ const Routes = new Router();
 Routes.get('/', (req, res) => res.send('Combinations-API - Endpoint padrão!'));
 
 Routes.post('/orderProcessor', CombinationController.store);
+Routes.get('/combinations-by-id', ControllerCombinationsById.handle)
 
 export default Routes;
