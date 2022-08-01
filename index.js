a850bdb953581282b406454e24d2d46a32461e98
rtof <zuichuan@msn.com>
2022-08-01 19:31:52 -0300

    previous version runing on https server

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
-// Import builtin NodeJS modules to instantiate the service
-const https = require("https");
-const fs = require("fs");
+import express from 'express';
+import https from 'https';
+import fs from 'fs';
 
-// Import the express module
-const express = require("express");
-
-// Instantiate an Express application
 const app = express();
 
-// Create a NodeJS HTTPS listener on port 4000 that points to the Express app
-// Use a callback function to tell when the server is created.
-https
-  .createServer(
-		// Provide the private and public key to the server by reading each
-		// file's content with the readFileSync() method.
-    {
-      key: fs.readFileSync("key.pem"),
-      cert: fs.readFileSync("cert.pem"),
-    },
-    app
-  )
-  .listen(3001, () => {
-    console.log("server is runing at port 3001");
-  });
+const port = 3000;
+const key = fs.readFileSync('./cert/key.pem');
+const cert = fs.readFileSync('./cert/cert.pem');
 
+const options = {
+  key: key,
+  cert: cert
+};
 
-// app.listen(3001, () => { console.log("server is runing at port 3001") });
+app.get('/', (_, res) => {
+   res.send('Now using https...');
+});
 
-// Create an try point route for the Express app listening on port 4000.
-// This code tells the service to listed to any request coming to the / route.
-// Once the request is received, it will display a message "Hello from express server."
-app.get('/', (_, res)=>{
-    res.send("Hello from express server https!")
-})
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+  console.log('server starting on port: ' + port)
+});
