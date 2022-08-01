d582acf6cc5b4037c2b7c5b92762afdac6b1ad47
Jean Fraga <jeandevjs@gmail.com>
2022-08-01 09:32:31 -0300

    refactor: remove commits

--- src/app/services/protocols/store-top-combinations.js
+++ src/app/services/protocols/store-top-combinations.js
@@ -78,88 +78,9 @@ export function mapStoreTopCombinations (objTopN) {
   return objTopN
 }
 
-//storeTopCombinations(2, [])
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*
-- topN object factory
-const N ;
-const { topN } = maketopN(N) 
-
-
--k = corresponde a key do topN 
-let k = 1
-
-- i = corresponde ao ponteiro esquerdo no topCombinations 
-- j = corresponde ao ponteiro direito no topCombinations
-let i = 0, j = 1;
-
-for (; j < topCombinations.length  ; j++) { 
-
-  - capturar as keys dos objetos no lado esquerdo e direito 
-  let leftKey = Object.keys(topCombinations[i])[0]
-  let rightKey = Object.keys(topCombinations[j])[0]
-
-  // ***Caso-1***
-  - Checar se leftKey e rightKey são iguais 
-  if ( leftKey === rightKey ) { 
-    - se sim, checar se o objeto é o ultimo elemento do topCombinations
-    if (j === topCombinations.length -1) {
-      - se sim, adicionar os values das keys dentro do array da key correspondente em topN
-      topN[k].push(topCombinations[i][leftKey])
-      topN[k].push(topCombinations[j][rightKey])
-
-    } else {
-      -  se não, adicionar e somente o value da esquerda
-      topN[k].push(topCombinations[i][leftKey])
-      - incrementar o ponteiro esquerdo
-      i++
-    }
-  }
-
-  // ***Caso-2***
-  - Checar se leftKey e rightKey são diferentes
-  if ( (leftKey !== rightKey)) {
-    
-    # se sim, checar se o objeto não é o último elemento do topCombinations
-    if (j !== topCombinations.length -1) {
-      - se sim adicionar o value da esquerda
-      topN[k].push(topCombinations[i][leftKey])
-      - incrementar ponteiro esquerdo
-      i++
-      // incrementar a key de topN
-      k += 1
-
-    } else {
-      # caso em que o objeto é o último elemento do topCombinations
-      - adicionar o value da esqueda  
-      topN[k].push(topCombinations[i][leftKey])
-      - incrementar a key de topN
-      k +=1
-      - adicionar o value da direita
-      topN[k].push(topCombinations[j][rightKey])
-      
-    }
-
-  } 
-
-}
-
-Example about points
-
   i
   3,3,3,2,2,2,1
     j
