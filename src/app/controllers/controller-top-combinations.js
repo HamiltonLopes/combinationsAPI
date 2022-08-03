73090416812ca137ed9caf1e7e8bf58ae66ed787
Jean Fraga <jeandevjs@gmail.com>
2022-08-03 10:08:07 -0300

    refactor: remove selection tops feature

--- src/app/controllers/controller-top-combinations.js
+++ src/app/controllers/controller-top-combinations.js
@@ -3,11 +3,9 @@ import {ServicesCombinations} from '../services/services-combinations.js'
 export class ControllerTopCombinations {
   static async handle (req, res) {
     try {
-      const { top } = req.body
-
       // Access Services to get topCombinations
       const combinations = new ServicesCombinations()
-      const topsCombinationsOfStore = await combinations.getStoreTopCombinantions(top)
+      const topsCombinationsOfStore = await combinations.getStoreTopCombinantions()
       return res.status(200).json(topsCombinationsOfStore)
 
     } catch (error) {
