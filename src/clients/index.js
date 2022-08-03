68fd023a610acba1e1540a087e7c81baf0a062fb
Jean Fraga <jeandevjs@gmail.com>
2022-08-03 16:07:48 -0300

    refactor: Add comments  about imports

--- src/clients/index.js
+++ src/clients/index.js
@@ -1,7 +1,7 @@
+/*
+  ALL EXPORTS IN A SINGLE FILE 
+*/
 export * from './client-get-order.js'
 export * from './client-catalog.js'
 export * from './client-hook.js'
 
-/*
-  ALL EXPORTS IN A SINGLE FILE 
-*/
