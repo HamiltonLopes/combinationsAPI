71db141f3dc4532609a06d4fb599f5413f67927c
Jean Fraga <jeandevjs@gmail.com>
2022-08-06 19:47:04 -0300

    refactor: fix bug

--- src/app/router.js
+++ src/app/router.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import CombinationController from '../presentation/controllers/controller-update-combinations.js'
+import {ControllerUpdateCombinations} from '../presentation/controllers/controller-update-combinations.js'
 import { ControllerCombinationsById } from '../presentation/controllers/controller-combinations-by-id.js'
 import { ControllerTopCombinations } from '../presentation/controllers/controller-top-combinations.js'
 // import dotenv from 'dotenv';
@@ -10,7 +10,7 @@ const Routes = new Router();
 
 Routes.get('/', (req, res) => res.send('Combinations-API - Endpoint padrão!'));
 
-Routes.post('/orderProcessor', CombinationController.store);
+Routes.post('/orderProcessor', ControllerUpdateCombinations.handle);
 Routes.get('/combinations-by-id/:productId', ControllerCombinationsById.handle)
 Routes.get('/store-top-combinations', ControllerTopCombinations.handle)
 
