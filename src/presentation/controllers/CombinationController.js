714fe8eb10915af4f7d706cdd40258f2ddbda77c
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-08-06 20:48:51 -0300

    bug correction

--- src/presentation/controllers/CombinationController.js
+++ src/presentation/controllers/CombinationController.js
@@ -48,35 +48,35 @@ export default new class CombinationController {
 
         if (items.length > 1) {
             for (const item of items) { //percorre os items do pedido recebido (a partir daqui itemPrincipal)
-                if (!combinations[item.id]) { //verifica se já existe combinações com o itemPrincipal
+                if (!combinations[item.productId]) { //verifica se já existe combinações com o itemPrincipal
                     combinations = { //se não existir, criar o objeto para inserir as combinacoes.
                         ...combinations,
-                        [item.id]: {
+                        [item.productId]: {
                             "combinations": []
                         }
                     }
                 }
                 for (const subItem of items) { //percorre novamente os items(a partir daqui subItem) do pedido para formar as combinações, para cada item ele deve combinar com todos os outros do pedido
-                    if (subItem.id !== item.id) { //verifica se o subItem a ser combinado não é o mesmo itemPrincipal
+                    if (subItem.productId !== item.productId) { //verifica se o subItem a ser combinado não é o mesmo itemPrincipal
                         let isInserted = false; //variavel para saber se a combinacao existe e foi acrescentada uma aparição
-                        for (let i = 0; i < combinations[item.id].combinations.length; i++) { //percorre a lista de combinações do itemPrincipal
-                            if (Object.values(combinations[item.id].combinations[i])[0] === subItem.id) { //verifica se existe a combinação do ItemPrincipal com o subItem
-                                const newApparitionsValue = +Object.keys(combinations[item.id].combinations[i])[0] + 1;
-                                combinations[item.id].combinations.push({  //se existir adiciona + 1 no número de aparições da combinação e insere no vetor de combinações
-                                    [newApparitionsValue]: subItem.id
+                        for (let i = 0; i < combinations[item.productId].combinations.length; i++) { //percorre a lista de combinações do itemPrincipal
+                            if (Object.values(combinations[item.productId].combinations[i])[0] === subItem.productId) { //verifica se existe a combinação do ItemPrincipal com o subItem
+                                const newApparitionsValue = +Object.keys(combinations[item.productId].combinations[i])[0] + 1;
+                                combinations[item.productId].combinations.push({  //se existir adiciona + 1 no número de aparições da combinação e insere no vetor de combinações
+                                    [newApparitionsValue]: subItem.productId
                                 });
-                                addTopCombination({ [item.id]: { [newApparitionsValue]: subItem.id } }); // adiciona a combinação nos tops, somando quando ela já existir ou verificando se a nova soma é superior
-                                combinations[item.id].combinations.splice(i, 1); //remove o número de combinações antigas(do itemPrincipal com o subItem) do vetor de combinações
-                                combinations[item.id].combinations = mergeSort(combinations[item.id].combinations); //Ordena novamente as combinações para a primeira combinação sempre ser a com maior número de aparições
+                                addTopCombination({ [item.productId]: { [newApparitionsValue]: subItem.productId } }); // adiciona a combinação nos tops, somando quando ela já existir ou verificando se a nova soma é superior
+                                combinations[item.productId].combinations.splice(i, 1); //remove o número de combinações antigas(do itemPrincipal com o subItem) do vetor de combinações
+                                combinations[item.productId].combinations = mergeSort(combinations[item.productId].combinations); //Ordena novamente as combinações para a primeira combinação sempre ser a com maior número de aparições
                                 isInserted = true; //informa que foi acrescentado uma aparição 
                                 break;
                             }
                         }
                         if (!isInserted) {
-                            combinations[item.id].combinations.push({  //se a combinacao não existir, insere ela no fim da lista com valor de uma aparição
-                                ["1"]: subItem.id
+                            combinations[item.productId].combinations.push({  //se a combinacao não existir, insere ela no fim da lista com valor de uma aparição
+                                ["1"]: subItem.productId
                             });
-                            addTopCombination({ [item.id]: { ["1"]: subItem.id } }); //tenta adicionar no array de melhoes combinacoes
+                            addTopCombination({ [item.productId]: { ["1"]: subItem.productId } }); //tenta adicionar no array de melhoes combinacoes
                         }
                     }
                 }
